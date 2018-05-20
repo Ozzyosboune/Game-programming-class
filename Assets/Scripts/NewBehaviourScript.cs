3c3df5131ff579fa718b2e783b208bf2281e89e0
Ozzyosboune <Ozzyosboune@users.noreply.github.com>
2018-05-20 14:36:05 -0700

    IT WORKS
    all 4 guns are firing no issue

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -25,15 +25,14 @@ public class NewBehaviourScript: MonoBehaviour
             GameObject Temporary_Bullet_Handler;
             Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
 
-            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.forward * 90);
 
             Rigidbody Temporary_RigidBody;
             Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
 
             //Set the bullet force
-            Temporary_RigidBody.AddForce(transform.up * Bullet_Forward_Force);
+            Temporary_RigidBody.AddForce(-transform.up * Bullet_Forward_Force);
 
             Destroy(Temporary_Bullet_Handler, 10.0f);
         }
